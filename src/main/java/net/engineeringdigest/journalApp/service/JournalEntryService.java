42565c64ba23afc81eefb95e77247d2ed10475ba
Iftikar Hussain <iftikarraza8@gmail.com>
2025-07-11 12:53:23 +0530

    Initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -0,0 +1,62 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+    private static final Logger log = LoggerFactory.getLogger(JournalEntryService.class);
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    // if any exception occurs in mid-way of executing this fun, any changes made before the error, will be rolled back
+    public void saveEntry(JournalEntry journalEntry, String userName) {
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        } catch (Exception e) {
+            log.error("Exception: ", e);
+            throw new RuntimeException("An error occurred while saving the entry!", e);
+        }
+
+    }
+
+    public void saveEntry(JournalEntry journalEntry) {
+        journalEntryRepository.save(journalEntry);
+    }
+
+
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id) {
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id, String userName) {
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
+        journalEntryRepository.deleteById(id);
+    }
+}
